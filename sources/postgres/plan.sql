022b1c38d5541101b42424a7f0c85a5e13a154e5
Matthew Bloch <mjbloch22@gmail.com>
2024-02-25 14:54:06 -0800

    Update dashboard

--- sources/postgres/plan.sql
+++ sources/postgres/plan.sql
@@ -1,4 +1,5 @@
-SELECT * 
+SELECT  *, 
+        plan.start_at::TIMESTAMP WITH TIME ZONE AT TIME ZONE 'America/Denver' AS local_ts
 FROM plan
 LEFT JOIN trails ON plan.trail_id = trails.id
 LEFT JOIN forecast ON plan.forecast_id = forecast.id;
